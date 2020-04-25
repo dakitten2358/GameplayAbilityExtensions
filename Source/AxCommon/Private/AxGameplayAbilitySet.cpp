8e5b4b781edee2ef2e6455843480331fa8979901
dakitten2358 <jay.w.murray@gmail.com>
2020-04-25 09:34:12 -0400

    Add AxGameplayAbilitySet (DataAsset) which can be used to give/remove abilities and effects

---
+++ Source/AxCommon/Private/AxGameplayAbilitySet.cpp
@@ -0,0 +1,99 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AxGameplayAbilitySet.h"
+#include "AxGameplayAbility.h"
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemGlobals.h"
+#include "AxCommonDebug.h"
+#include "GameFramework/Actor.h"
+
+void UAxGameplayAbilitySet::Give(UAbilitySystemComponent* AbilitySystemComponent, UObject* SourceObject, int32 Level, FAxGameplayAbilitySetHandles& OutAbilitySetHandles) const
+{
+	if (AbilitySystemComponent == nullptr)
+	{
+		UE_LOG(LogAxCommon, Error, TEXT("Tried to give AxGameplayAbilitySet to a null AbilitySystemComponent"));
+		return;
+	}
+
+	for (const TSubclassOf<UGameplayAbility>& Ability : Abilities)
+	{
+		int32 inputID = INDEX_NONE;
+		UAxGameplayAbility* AxAbility = Cast<UAxGameplayAbility>(Ability.GetDefaultObject());
+		if (AxAbility)
+		{
+			inputID = static_cast<int32>(AxAbility->GetAbilityInputID());
+		}
+
+		FGameplayAbilitySpecHandle AbilitySpecHandle = AbilitySystemComponent->GiveAbility(FGameplayAbilitySpec(Ability, Level, inputID, SourceObject));
+		OutAbilitySetHandles.AbilityHandles.Add(AbilitySpecHandle);
+	}
+
+	for (const TSubclassOf<UGameplayEffect>& GameplayEffect : Effects)
+	{
+		FGameplayEffectContextHandle EffectContext = AbilitySystemComponent->MakeEffectContext();
+		if (SourceObject) EffectContext.AddSourceObject(SourceObject);
+
+		FGameplayEffectSpecHandle NewHandle = AbilitySystemComponent->MakeOutgoingSpec(GameplayEffect, Level, EffectContext);
+		if (NewHandle.IsValid())
+		{
+			FActiveGameplayEffectHandle ActiveGEHandle = AbilitySystemComponent->ApplyGameplayEffectSpecToTarget(*NewHandle.Data.Get(), AbilitySystemComponent);
+			OutAbilitySetHandles.EffectHandles.Add(ActiveGEHandle);
+		}
+	}
+}
+
+void UAxGameplayAbilitySet::Give(AActor* Actor, UObject* SourceObject, int32 Level, FAxGameplayAbilitySetHandles& OutAbilitySetHandles) const
+{
+	Give(UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(Actor), SourceObject, Level, OutAbilitySetHandles);
+}
+
+void UAxGameplayAbilitySet::Give(UAbilitySystemComponent* AbilitySystemComponent, UObject* SourceObject, int32 Level) const
+{
+	FAxGameplayAbilitySetHandles DiscardHandles;
+	Give(AbilitySystemComponent, SourceObject, Level, DiscardHandles);
+}
+
+void UAxGameplayAbilitySet::Give(AActor* Actor, UObject* SourceObject, int32 Level) const
+{
+	Give(UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(Actor), SourceObject, Level);
+}
+
+void UAxGameplayAbilitySet::Remove(UAbilitySystemComponent* AbilitySystemComponent, FAxGameplayAbilitySetHandles& AbilitySetHandles) const
+{
+	if (AbilitySystemComponent == nullptr)
+	{
+		UE_LOG(LogAxCommon, Error, TEXT("Tried to remove AxGameplayAbilitySet from a null AbilitySystemComponent"));
+		return;
+	}
+
+	if (AbilitySetHandles.EffectHandles.Num() > 0)
+	{
+		// we'll only remove the abilities that are actually removed
+		TArray<FActiveGameplayEffectHandle>& EffectHandles = AbilitySetHandles.EffectHandles;
+		for (int EffectIndex = EffectHandles.Num() - 1; EffectIndex >= 0; --EffectIndex)
+		{
+			const FActiveGameplayEffectHandle& EffectHandle = EffectHandles[EffectIndex];
+			if (AbilitySystemComponent->RemoveActiveGameplayEffect(EffectHandle))
+			{
+				EffectHandles.RemoveAt(EffectIndex);
+			}
+		}
+	}
+
+	if (AbilitySetHandles.AbilityHandles.Num() > 0)
+	{
+		// there's no way for us to know if it was actually removed, so we'll empty them all,
+		// and then just empty the list
+		for (const FGameplayAbilitySpecHandle& AbilityHandle : AbilitySetHandles.AbilityHandles)
+		{
+			AbilitySystemComponent->ClearAbility(AbilityHandle);
+		}
+		AbilitySetHandles.AbilityHandles.Empty();
+	}
+}
+
+void UAxGameplayAbilitySet::Remove(AActor* Actor, FAxGameplayAbilitySetHandles& AbilitySetHandles) const
+{
+	Remove(UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(Actor), AbilitySetHandles);
+}
---
+++ Source/AxCommon/Public/AxGameplayAbilitySet.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "GameplayAbilitySpec.h"
+#include "GameplayEffectTypes.h"
+#include "AxGameplayAbilitySet.generated.h"
+
+// Similar to UGameplayAbilitySet, but removes the bindings,
+// adds start up effects, and if it's a UAxGameplayAbility
+// will use the binding from that.  Also provides a way to
+// remove the abilities and effects (useful for weapons,
+// equippables, etc)
+
+USTRUCT()
+struct FAxGameplayAbilitySetHandles
+{
+	GENERATED_BODY()
+
+public:
+	bool Any() const { return (AbilityHandles.Num() > 0) || (EffectHandles.Num() > 0); }
+
+protected:
+	UPROPERTY()
+	TArray<FGameplayAbilitySpecHandle> AbilityHandles;
+
+	UPROPERTY()
+	TArray<FActiveGameplayEffectHandle> EffectHandles;
+
+	friend class UAxGameplayAbilitySet;
+};
+
+UCLASS()
+class AXCOMMON_API UAxGameplayAbilitySet : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	void Give(class UAbilitySystemComponent* AbilitySystemComponent, UObject* SourceObject, int32 Level, FAxGameplayAbilitySetHandles& OutAbilitySetHandles) const;
+	void Give(class AActor* Actor, UObject* SourceObject, int32 Level, FAxGameplayAbilitySetHandles& OutAbilitySetHandles) const;
+	void Give(class UAbilitySystemComponent* AbilitySystemComponent, UObject* SourceObject = nullptr, int32 Level = 1) const;
+	void Give(class AActor* Actor, UObject* SourceObject = nullptr, int32 Level = 1) const;
+
+	void Remove(class UAbilitySystemComponent* AbilitySystemComponent, FAxGameplayAbilitySetHandles& AbilitySetHandles) const;
+	void Remove(class AActor* Actor, FAxGameplayAbilitySetHandles& AbilitySetHandles) const;
+
+protected:
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Abilities")
+	TArray<TSubclassOf<class UGameplayAbility>> Abilities;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Effects")
+	TArray<TSubclassOf<class UGameplayEffect>> Effects;
+};
