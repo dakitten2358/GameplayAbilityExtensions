8f7f8e9236804ca68166eae4fd8cbef92decd305
dakitten2358 <jay.w.murray@gmail.com>
2020-04-26 08:31:47 -0400

    Add helper macros for treating attributes as an int (rounded)

--- Source/AxCommon/Public/AttributeSetHelpers.h
+++ Source/AxCommon/Public/AttributeSetHelpers.h
@@ -9,3 +9,33 @@
 	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
 	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
 	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
+// For ints
+#define GAMEPLAYATTRIBUTE_VALUE_INT_GETTER(PropertyName) \
+	FORCEINLINE int32 Get##PropertyName() const \
+	{ \
+		return static_cast<int32>(PropertyName.GetCurrentValue()); \
+	}
+
+#define GAMEPLAYATTRIBUTE_VALUE_INT_SETTER(PropertyName) \
+	FORCEINLINE void Set##PropertyName(int32 NewVal) \
+	{ \
+		UAbilitySystemComponent* AbilityComp = GetOwningAbilitySystemComponent(); \
+		if (ensure(AbilityComp)) \
+		{ \
+			AbilityComp->SetNumericAttributeBase(Get##PropertyName##Attribute(), NewVal); \
+		}; \
+	}
+
+#define GAMEPLAYATTRIBUTE_VALUE_INT_INITTER(PropertyName) \
+	FORCEINLINE void Init##PropertyName(int32 NewVal) \
+	{ \
+		PropertyName.SetBaseValue(NewVal); \
+		PropertyName.SetCurrentValue(NewVal); \
+	}
+
+#define ATTRIBUTE_INT_ACCESSORS(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INT_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INT_SETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INT_INITTER(PropertyName)
