0faf02778d2a6c10861d23cd9b8433352e7de97f
dakitten2358 <jay.w.murray@gmail.com>
2020-05-02 09:26:57 -0400

    Add support for auto-activated abilities

--- Source/AxCommon/Private/AxGameplayAbility.cpp
+++ Source/AxCommon/Private/AxGameplayAbility.cpp
@@ -1,6 +1,24 @@
 #include "AxGameplayAbility.h"
+#include "AbilitySystemComponent.h"
+#include "Abilities/GameplayAbilityTypes.h"
+#include "AxCommonDebug.h"
+#include "GameFramework/Actor.h"
 
 int32 UAxGameplayAbility::GetAbilityInputID() const
 {
 	return INDEX_NONE;
+}
+
+void UAxGameplayAbility::OnAvatarSet(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec)
+{
+	Super::OnAvatarSet(ActorInfo, Spec);
+
+	if (bActivateAbilityOnGranted)
+	{
+		bool ActivatedAbility = ActorInfo->AbilitySystemComponent->TryActivateAbility(Spec.Handle, false);
+		if (!ActivatedAbility)
+		{
+			UE_LOG(LogAxCommon, Warning, TEXT("Failed to auto-activated ability on %s"), *ActorInfo->OwnerActor->GetName());
+		}
+	}
 }
--- Source/AxCommon/Public/AxGameplayAbility.h
+++ Source/AxCommon/Public/AxGameplayAbility.h
@@ -17,4 +17,13 @@ class AXCOMMON_API UAxGameplayAbility : public UGameplayAbility
 public:
 	// must be implemented in derived classes!
 	virtual int32 GetAbilityInputID() const;
+
+	// If an ability is marked as 'ActivateAbilityOnGranted', activate them immediately when given here
+	// Epic's comment: Projects may want to initiate passives or do other "BeginPlay" type of logic here.
+	virtual void OnAvatarSet(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec) override;
+
+public:
+	// Tells an ability to activate immediately when its granted. Used for passive abilities and abilites forced on others.
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Ability")
+	bool bActivateAbilityOnGranted;
 };
