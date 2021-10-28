96bfa1f5a0d173518c969bb23cda7cf6efdef185
dakitten2358 <jay.w.murray@gmail.com>
2021-10-28 19:50:00 -0400

    debug for interactions

---
+++ Source/AxInteractions/Private/AxInteractionsDebug.cpp
@@ -0,0 +1,4 @@
+#include "AxInteractionsDebug.h"
+#include "Engine/Engine.h"
+
+DEFINE_LOG_CATEGORY(LogAxInteractions);
---
+++ Source/AxInteractions/Private/AxInteractionsDebug.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AxCommonDrawDebug.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogAxInteractions, Log, All);
