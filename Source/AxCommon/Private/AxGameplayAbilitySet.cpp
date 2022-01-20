5fa699a055dc788d62bf0419ee6104b7e6375167
dakitten2358 <jay.w.murray@gmail.com>
2022-01-19 21:15:46 -0500

    split UAxGameplayAbilitySet's data stuff out to FAxGameplayAbilitySetData for ease of use in one-off objects; it felt weird to have to make an ability set for each object, and then link it to the object

--- Source/AxCommon/Private/AxGameplayAbilitySet.cpp
+++ Source/AxCommon/Private/AxGameplayAbilitySet.cpp
@@ -17,42 +17,8 @@ void UAxGameplayAbilitySet::Give(UAbilitySystemComponent* AbilitySystemComponent
 		return;
 	}
 
-	for (const TSubclassOf<UGameplayAbility>& AbilityClass : Abilities)
-	{
-		if (!AbilityClass)
-		{
-			UE_LOG(LogAxCommon, Error, TEXT("Tried to give a null ability while granting abilities to %s"), *AActor::GetDebugName(AbilitySystemComponent->GetOwner()));
-			continue;
-		}
-		int32 inputID = INDEX_NONE;
-		FGameplayAbilitySpec AbilitySpec = FGameplayAbilitySpec(AbilityClass, Level, inputID, SourceObject);
-		AbilitySpec.RemoveAfterActivation = bRemoveAbilitiesAfterActivation;
-		FGameplayAbilitySpecHandle AbilitySpecHandle = AbilitySystemComponent->GiveAbility(AbilitySpec);
-		OutAbilitySetHandles.AbilityHandles.Add(AbilitySpecHandle);
-	}
-
-	for (const TSubclassOf<UGameplayEffect>& GameplayEffectClass : Effects)
-	{
-		if (!GameplayEffectClass)
-		{
-			UE_LOG(LogAxCommon, Error, TEXT("Tried to give a null ability while granting abilities to %s"), *AActor::GetDebugName(AbilitySystemComponent->GetOwner()));
-			continue;
-		}
-
-		FGameplayEffectContextHandle EffectContext = AbilitySystemComponent->MakeEffectContext();
-		if (SourceObject) EffectContext.AddSourceObject(SourceObject);
-
-		FGameplayEffectSpecHandle NewHandle = AbilitySystemComponent->MakeOutgoingSpec(GameplayEffectClass, Level, EffectContext);
-		if (NewHandle.IsValid())
-		{
-			FActiveGameplayEffectHandle ActiveGEHandle = AbilitySystemComponent->ApplyGameplayEffectSpecToTarget(*NewHandle.Data.Get(), AbilitySystemComponent);
-
-			// we only keep track of EGameplayEffectDurationType::Infinite effects, we'll assume ::Instant and ::HasDuration
-			// we'll sort themselves out
-			if (GameplayEffectClass.GetDefaultObject()->DurationPolicy == EGameplayEffectDurationType::Infinite)
-				OutAbilitySetHandles.EffectHandles.Add(ActiveGEHandle);
-		}
-	}
+	FAxGameplayAbilitySetData::GiveAbilities(AbilitySystemComponent, SourceObject, Level, bRemoveAbilitiesAfterActivation, Abilities, OutAbilitySetHandles);
+	FAxGameplayAbilitySetData::GiveEffects(AbilitySystemComponent, SourceObject, Level, Effects, OutAbilitySetHandles);
 }
 
 void UAxGameplayAbilitySet::Give(AActor* Actor, UObject* SourceObject, int32 Level, FAxGameplayAbilitySetHandles& OutAbilitySetHandles) const
@@ -79,6 +45,114 @@ void UAxGameplayAbilitySet::Remove(UAbilitySystemComponent* AbilitySystemCompone
 		return;
 	}
 
+	FAxGameplayAbilitySetData::RemoveEffects(AbilitySystemComponent, AbilitySetHandles);
+	FAxGameplayAbilitySetData::RemoveAbilities(AbilitySystemComponent, AbilitySetHandles);
+}
+
+void UAxGameplayAbilitySet::Remove(AActor* Actor, FAxGameplayAbilitySetHandles& AbilitySetHandles) const
+{
+	Remove(UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(Actor), AbilitySetHandles);
+}
+
+void FAxGameplayAbilitySetData::Give(UAbilitySystemComponent* AbilitySystemComponent, UObject* SourceObject, int32 Level, FAxGameplayAbilitySetHandles& OutAbilitySetHandles) const
+{
+	if (AbilitySystemComponent == nullptr)
+	{
+		UE_LOG(LogAxCommon, Error, TEXT("Tried to give FAxGameplayAbilitySetData to a null AbilitySystemComponent"));
+		return;
+	}
+
+	const bool bRemoveAbilitiesAfterActivation = false;
+	FAxGameplayAbilitySetData::GiveAbilities(AbilitySystemComponent, SourceObject, Level, bRemoveAbilitiesAfterActivation, Abilities, OutAbilitySetHandles);
+	FAxGameplayAbilitySetData::GiveEffects(AbilitySystemComponent, SourceObject, Level, Effects, OutAbilitySetHandles);
+}
+
+void FAxGameplayAbilitySetData::Give(AActor* Actor, UObject* SourceObject, int32 Level, FAxGameplayAbilitySetHandles& OutAbilitySetHandles) const
+{
+	Give(UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(Actor), SourceObject, Level, OutAbilitySetHandles);
+}
+
+void FAxGameplayAbilitySetData::Remove(UAbilitySystemComponent* AbilitySystemComponent, FAxGameplayAbilitySetHandles& AbilitySetHandles) const
+{
+	if (!IsValid(AbilitySystemComponent))
+	{
+		UE_LOG(LogAxCommon, Error, TEXT("Tried to remove FAxGameplayAbilitySetData from a null AbilitySystemComponent"));
+		return;
+	}
+
+	FAxGameplayAbilitySetData::RemoveEffects(AbilitySystemComponent, AbilitySetHandles);
+	FAxGameplayAbilitySetData::RemoveAbilities(AbilitySystemComponent, AbilitySetHandles);
+}
+
+void FAxGameplayAbilitySetData::Remove(AActor* Actor, FAxGameplayAbilitySetHandles& AbilitySetHandles) const
+{
+	Remove(UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(Actor), AbilitySetHandles);
+}
+
+bool FAxGameplayAbilitySetData::Any() const
+{
+	return Abilities.Num() > 0 || Effects.Num() > 0;
+}
+
+void FAxGameplayAbilitySetData::GiveAbilities(class UAbilitySystemComponent* AbilitySystemComponent, UObject* SourceObject, int32 Level, bool bRemoveAbilitiesAfterActivation, const TArray<TSubclassOf<class UGameplayAbility>>& Abilities, FAxGameplayAbilitySetHandles& OutAbilitySetHandles)
+{
+	for (const TSubclassOf<UGameplayAbility>& AbilityClass : Abilities)
+	{
+		if (!AbilityClass)
+		{
+			UE_LOG(LogAxCommon, Error, TEXT("Tried to give a null ability while granting abilities to %s"), *AActor::GetDebugName(AbilitySystemComponent->GetOwner()));
+			continue;
+		}
+		int32 inputID = INDEX_NONE;
+		FGameplayAbilitySpec AbilitySpec = FGameplayAbilitySpec(AbilityClass, Level, inputID, SourceObject);
+		AbilitySpec.RemoveAfterActivation = bRemoveAbilitiesAfterActivation;
+		FGameplayAbilitySpecHandle AbilitySpecHandle = AbilitySystemComponent->GiveAbility(AbilitySpec);
+		OutAbilitySetHandles.AbilityHandles.Add(AbilitySpecHandle);
+	}
+}
+
+void FAxGameplayAbilitySetData::GiveEffects(class UAbilitySystemComponent* AbilitySystemComponent, UObject* SourceObject, int32 Level, const TArray<TSubclassOf<class UGameplayEffect>>& Effects, FAxGameplayAbilitySetHandles& OutAbilitySetHandles)
+{
+	for (const TSubclassOf<UGameplayEffect>& GameplayEffectClass : Effects)
+	{
+		if (!GameplayEffectClass)
+		{
+			UE_LOG(LogAxCommon, Error, TEXT("Tried to give a null ability while granting abilities to %s"), *AActor::GetDebugName(AbilitySystemComponent->GetOwner()));
+			continue;
+		}
+
+		FGameplayEffectContextHandle EffectContext = AbilitySystemComponent->MakeEffectContext();
+		if (SourceObject) EffectContext.AddSourceObject(SourceObject);
+
+		FGameplayEffectSpecHandle NewHandle = AbilitySystemComponent->MakeOutgoingSpec(GameplayEffectClass, Level, EffectContext);
+		if (NewHandle.IsValid())
+		{
+			FActiveGameplayEffectHandle ActiveGEHandle = AbilitySystemComponent->ApplyGameplayEffectSpecToTarget(*NewHandle.Data.Get(), AbilitySystemComponent);
+
+			// we only keep track of EGameplayEffectDurationType::Infinite effects, we'll assume ::Instant and ::HasDuration
+			// we'll sort themselves out
+			if (GameplayEffectClass.GetDefaultObject()->DurationPolicy == EGameplayEffectDurationType::Infinite)
+				OutAbilitySetHandles.EffectHandles.Add(ActiveGEHandle);
+		}
+	}
+}
+
+void FAxGameplayAbilitySetData::RemoveAbilities(class UAbilitySystemComponent* AbilitySystemComponent, FAxGameplayAbilitySetHandles& AbilitySetHandles)
+{
+	if (AbilitySetHandles.AbilityHandles.Num() > 0)
+	{
+		// there's no way for us to know if it was actually removed, so we'll empty them all,
+		// and then just empty the list
+		for (const FGameplayAbilitySpecHandle& AbilityHandle : AbilitySetHandles.AbilityHandles)
+		{
+			AbilitySystemComponent->ClearAbility(AbilityHandle);
+		}
+		AbilitySetHandles.AbilityHandles.Empty();
+	}
+}
+
+void FAxGameplayAbilitySetData::RemoveEffects(class UAbilitySystemComponent* AbilitySystemComponent, FAxGameplayAbilitySetHandles& AbilitySetHandles)
+{
 	if (AbilitySetHandles.EffectHandles.Num() > 0)
 	{
 		// we'll only remove the abilities that are actually removed
@@ -92,20 +166,4 @@ void UAxGameplayAbilitySet::Remove(UAbilitySystemComponent* AbilitySystemCompone
 			}
 		}
 	}
-
-	if (AbilitySetHandles.AbilityHandles.Num() > 0)
-	{
-		// there's no way for us to know if it was actually removed, so we'll empty them all,
-		// and then just empty the list
-		for (const FGameplayAbilitySpecHandle& AbilityHandle : AbilitySetHandles.AbilityHandles)
-		{
-			AbilitySystemComponent->ClearAbility(AbilityHandle);
-		}
-		AbilitySetHandles.AbilityHandles.Empty();
-	}
-}
-
-void UAxGameplayAbilitySet::Remove(AActor* Actor, FAxGameplayAbilitySetHandles& AbilitySetHandles) const
-{
-	Remove(UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(Actor), AbilitySetHandles);
 }
--- Source/AxCommon/Public/AxGameplayAbilitySet.h
+++ Source/AxCommon/Public/AxGameplayAbilitySet.h
@@ -28,6 +28,7 @@ protected:
 	TArray<FActiveGameplayEffectHandle> EffectHandles;
 
 	friend class UAxGameplayAbilitySet;
+	friend struct FAxGameplayAbilitySetData;
 };
 
 UCLASS()
@@ -54,3 +55,38 @@ protected:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Abilities")
 	bool bRemoveAbilitiesAfterActivation;
 };
+
+// helper so that we don't always need to create a data asset; I found that i was often
+// creating one-off data assets for a single object
+USTRUCT(BlueprintType)
+struct AXCOMMON_API FAxGameplayAbilitySetData
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Abilities")
+	TArray<TSubclassOf<class UGameplayAbility>> Abilities;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Effects")
+	TArray<TSubclassOf<class UGameplayEffect>> Effects;
+
+	void Give(class UAbilitySystemComponent* AbilitySystemComponent, UObject* SourceObject, int32 Level, FAxGameplayAbilitySetHandles& OutAbilitySetHandles) const;
+	void Give(class AActor* Actor, UObject* SourceObject, int32 Level, FAxGameplayAbilitySetHandles& OutAbilitySetHandles) const;
+	
+	void Remove(class UAbilitySystemComponent* AbilitySystemComponent, FAxGameplayAbilitySetHandles& AbilitySetHandles) const;
+	void Remove(class AActor* Actor, FAxGameplayAbilitySetHandles& AbilitySetHandles) const;
+
+	bool Any() const;
+
+private:
+	// these are part of this struct rather than free floating functions so that we can get access to the private members
+	// of FAxGameplayAbilitySetHandles
+	static void GiveAbilities(class UAbilitySystemComponent* AbilitySystemComponent, UObject* SourceObject, int32 Level, bool bRemoveAbilitiesAfterActivation, const TArray<TSubclassOf<class UGameplayAbility>>& Abilities, FAxGameplayAbilitySetHandles& OutAbilitySetHandles);
+	static void GiveEffects(class UAbilitySystemComponent* AbilitySystemComponent, UObject* SourceObject, int32 Level, const TArray<TSubclassOf<class UGameplayEffect>>& Effects, FAxGameplayAbilitySetHandles& OutAbilitySetHandles);
+
+	static void RemoveAbilities(class UAbilitySystemComponent* AbilitySystemComponent, FAxGameplayAbilitySetHandles& AbilitySetHandles);
+	static void RemoveEffects(class UAbilitySystemComponent* AbilitySystemComponent, FAxGameplayAbilitySetHandles& AbilitySetHandles);
+
+	friend class UAxGameplayAbilitySet;
+};
+
